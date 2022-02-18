a11f976e91f69f30d2fb118979de43f98f03404d
Pavitra jha <86011141+pavitra1129@users.noreply.github.com>
2022-02-18 18:42:36 +0530

    Create indentify.py

---
+++ indentify.py
@@ -0,0 +1,7 @@
+print("this is a simple python script Made BY:MR.PAVITRA JHA")
+input1 = input("first number: ")
+input2 = input("second number: ")
+print("is the first number smaller than the second number: " , input1<input2)
+print("is the second number bigger than the first number: " , input2<input1)
+print("are they both equal:" , input1==input2)
+# <,>,==
